b2f2355664a967b79ee21e2c61caebb3e70788c2
Samaritan-Machine <abhidasari.cloud1@gmail.com>
2021-11-11 21:24:57 -0500

    test upload1

--- lambda_function.py
+++ lambda_function.py
@@ -3,6 +3,7 @@ import boto3
 import requests
 
 def detect_labels(photo, bucket):
+    #test upload2
     labels_res = []
     client=boto3.client('rekognition')
     response = client.detect_labels(Image={'S3Object':{'Bucket':bucket,'Name':photo}},
